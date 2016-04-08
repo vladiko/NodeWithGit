de1fd0dee485017830a15b9aaed79dd00902e022
vladiko <koganvladimir@yahoo.com>
2016-04-08 13:45:18 +0300

    commit for server.ts change

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change tesbt w sdsss2\n');
+    res.end('Hello World change test for this version\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change tesbt w sdsss2\n');
+    res.end('Hello World change test for this version\n');
 }).listen(port);
