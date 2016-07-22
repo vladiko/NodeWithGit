7498c65057a65480faec0da132f8dfc84d3a0707
vladiko <koganvladimir@yahoo.com>
2016-07-22 15:35:44 +0300

    serviset next test

--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change test for this versionb aa \n');
+    res.end('Hello World change test for this versionb a \n');
 }).listen(port);
