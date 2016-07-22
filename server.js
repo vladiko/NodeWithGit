fa32943d3444d734ba20fb5084939c0fa96f188c
vladiko <koganvladimir@yahoo.com>
2016-07-22 16:12:52 +0300

    test outgoing commints

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change test for this versionb a \n');
+    res.end('Hello World change test for this versionbss a \n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
