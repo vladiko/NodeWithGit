9189c787e4eb1919c69bb46c70d4feb05e895ba8
vladiko <koganvladimir@yahoo.com>
2016-07-22 16:23:47 +0300

    test smart git

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change test for this version 3 \n');
+    res.end('Hello World change test for this version 3  test smart git\n');
 }).listen(port);
+s;
 //# sourceMappingURL=server.js.map
