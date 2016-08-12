69f78e315e6f628ba3d2d063ce9302c2e5c51aa3
Vladimir, Kogan <vladimir_kogan@bmc.com>
2016-08-12 18:06:42 +0300

    test of change

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change test for this version 3  test smart git\n');
+    res.end('Hello World change test for this version 3  test smart git 2\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
